23ec31c02217d758a4a7f51266c5fd3be3d952ae
AlexDev <yofranksalas@gmail.com>
2025-03-31 14:29:35 -0400

    Actualizacion de alertas

--- app/views/js/ajax.js
+++ app/views/js/ajax.js
@@ -16,9 +16,69 @@ formularios_ajax.forEach(formularios => {
             cancelButtonText: 'Cancelar!',
         }).then((result) => {
             if (result.isConfirmed) {
+
+                let data = new FormData(this);
+                let method = this.getAttribute("method");
+                let action = this.getAttribute("action");
+
+                let encabezados = new Headers();
+
+                let config = {
+                    method: method,
+                    headers: encabezados,
+                    mode:'cors',
+                    cache:'no-cache',
+                    body: data,
+                };
+
+                fetch(action,config)
+                .then(respuesta => respuesta.json())
+                .then(respuesta =>{
+                    return alertas_ajax(respuesta);
+                });
               
             }
         })
 
     });
 });
+
+function alertas_ajax(alerta){
+
+    if(alerta.tipo=="simple"){
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+        });
+        
+    }else if(alerta.tipo=="recargar"){
+
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+          }).then((result) => {
+            if(result.isConfirmed){
+                location.reload();
+            }
+          });
+    }else if(alerta.tipo=="limpiar"){
+
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+          }).then((result) => {
+            if(result.isConfirmed){
+                document.querySelector(".FormularioAjax").reset();
+            }
+          });
+    }else if(alerta.tipo=="redireccionar"){
+        window.location.href=alerta.url;
+
+    }
+};
