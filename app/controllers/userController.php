a69e5307c03ab5ceb731078014dad928d0a95bc6
AlexDev <yofranksalas@gmail.com>
2025-04-05 15:00:16 -0400

    Funciones para subir imagenes al servidor

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -124,6 +124,74 @@
             }
             # Directorio de imagenes #
             $img_dir="../views/fotos/";
+
+            # Comprobar si se ha seleccionado una imagen #
+            if($_FILES['usuario_foto']['name']!="" && $_FILES['usuario_foto']['size']>0){
+
+                # Creando directorios #
+                if(!file_exists($img_dir)){
+                    if(!mkdir($img_dir,0777)){
+                        $alerta=[
+                            "tipo"=>"simple",
+                            "titulo"=>"Ocurrio un error inesperado",
+                            "texto"=>"Error al crear el directorio",
+                            "icono"=>"error"
+                        ];
+                        return json_encode($alerta);
+                        exit();
+                    }
+                }
+                # Verificando el formato de imagenes #
+                if(mime_content_type($_FILES['usuario_foto']['tmp_name'])!="image/jpeg" && mime_content_type($_FILES['usuario_foto']['tmp_name'])!="image/png"){
+                    $alerta=[
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrio un error inesperado",
+                        "texto"=>"La imagen que se ha seleccionado es de un FORMATO NO PERMITIDO",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();
+                }
+                # Verificando peso de la imagen #
+                if(($_FILES['usuario_foto']['size']/1024)>5120){
+                    $alerta=[
+                        "tipo"=>"simple",
+                        "titulo"=>"Ocurrio un error inesperado",
+                        "texto"=>"La imagen que ha seleccionado SUPERA el peso PERMITIDO",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();
+                }
+                # Nombre de la foto #
+                $foto=str_ireplace(" ","_",$nombre);
+                $foto=$foto."_".rand(0,100);
+
+                # Extension de la imagen #
+                switch(mime_content_type($_FILES['usuario_foto']['tmp_name'])){
+                    case "image/jpeg":
+                        $foto=$foto.".jpeg";
+                        break;
+                    case "image/png":
+                        $foto=$foto.".png";
+                        break;
+                }
+                # chmod da permisos de escritura y lectura, en este caso al directorio que se almacena en la variable #
+                chmod($img_dir,0777);
+                # Moviendo imagen al directorio #
+                if(!move_uploaded_file($_FILES['usuario_foto']['tmp_name'],$img_dir.$foto)){
+                    $alerta=[
+                        "tipo"=>"simple",
+                        "titulo"=>"ERROR AL MOVER LA IMAGEN",
+                        "texto"=>"No se pudo subir la imagen al sistema en este momento",
+                        "icono"=>"error"
+                    ];
+                    return json_encode($alerta);
+                    exit();
+                }
+            }else{
+                $foto="";
+            }
         }
         
     }
