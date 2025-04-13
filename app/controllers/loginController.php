446712bc67aaf9f2ae9ea2fef6fa29181b7ecf6b
AlexDev <yofranksalas@gmail.com>
2025-04-13 14:05:01 -0400

    Inicio de Sesion terminado

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -51,7 +51,53 @@
                         </script>
                         ";
                     }else{
+                      # Verificando usuario #
+                      $check_usuario=$this->ejecutarConsulta("SELECT * FROM usuario WHERE usuario_usuario='$usuario'");
 
+                      if($check_usuario->rowCount()==1){
+                        # VERIFICANDO QUE LOS CAMPOS DE USUARIO Y CONTRASENA DE MI BASE DE DATOS SEAN CORRECTOS
+                        $check_usuario=$check_usuario->fetch();
+                        if($check_usuario['usuario_usuario']== $usuario && password_verify($clave, $check_usuario['usuario_clave'])){
+
+                          $_SESSION['id']=$check_usuario['usuario_id'];
+                          $_SESSION['nombre']=$check_usuario['usuario_nombre'];
+                          $_SESSION['apellido']=$check_usuario['usuario_apellido'];
+                          $_SESSION['usuario']=$check_usuario['usuario_usuario'];
+                          $_SESSION['foto']=$check_usuario['usuario_foto'];
+
+                          if(headers_sent()){
+                            echo"<script> window.location.href='".APP_URL."dashboard/';
+                            </script>";
+                          }else{
+                            header("Location: ".APP_URL."dashboard/");
+                          }
+
+
+                        }else{
+                          echo"
+                          <script>
+                          Swal.fire({
+                              icon: 'error',
+                              title: 'Ocurrio un error inesperado',
+                              text: 'EL USUARIO O CLAVE SON INCORRECTOS',
+                              confirmButtonText: 'Aceptar'
+                              });
+                            </script>
+                            ";
+                        }
+                      
+                      }else{
+                          echo"
+                          <script>
+                          Swal.fire({
+                              icon: 'error',
+                              title: 'Ocurrio un error inesperado',
+                              text: 'EL USUARIO O CLAVE SON INCORRECTOS',
+                              confirmButtonText: 'Aceptar'
+                              });
+                            </script>
+                            ";
+                      }
                     }
                 }
               }
